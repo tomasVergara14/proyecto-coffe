5268e323ed7fbf10b8fb8ed94f8f5d6061d936a6
tomasVergara14 <tomasvergara272@gmail.com>
2021-07-15 12:30:31 -0300

    entrega componentes 2

--- src/components/itemCount/ItemCount.jsx
+++ src/components/itemCount/ItemCount.jsx
@@ -7,6 +7,22 @@ const ItemCount = (props) => {
 
     const {initial,stockItem} = props
     const [onAdd, setAdd] = useState(Number(initial))  
+
+    const add = ()=>{if(!stockItem){
+            setAdd("0")
+        }else if(onAdd < stockItem  ){
+            setAdd(onAdd +1)
+        }
+        return onAdd}
+    
+    const rest = ()=>{if( onAdd > initial ){
+            setAdd(onAdd - 1)
+        }
+    }
+
+    const cart = ()=>{ alert(`Esta agregando: ${onAdd} productos`) }
+
+    
     
 
     return (
@@ -16,25 +32,12 @@ const ItemCount = (props) => {
             <form className="itemCount" action="">
                 <div className="countButtons">
                 
-                    <button className="Button" type="button" onClick={
-                        ()=>{if( onAdd > initial ){
-                            setAdd(onAdd - 1)
-                        }} 
-                    }> - </button>
+                    <button className="Button" type="button" onClick={rest}> - </button>
                     <p>{onAdd}</p>
-                    <button className="Button" type="button" onClick={
-                        ()=>{if(!stockItem){
-                            setAdd("0")
-                        }else if(onAdd < stockItem  ){
-                            setAdd(onAdd +1)
-                        }
-                    return onAdd} 
-                    }>+</button>
+                    <button className="Button" type="button" onClick={add}>+</button>
                     
                 </div>
-                <button className="Add" type="button" onClick={
-                    ()=>{ alert(`Esta agregando: ${onAdd} productos`) }
-                } >Agregar al carrito</button>    
+                <button className="Add" type="button" onClick={cart} >Agregar al carrito</button>    
             </form>
             
             
