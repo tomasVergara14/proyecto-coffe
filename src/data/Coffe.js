c8aff05795681ff364b9a40e5e7aa6c8d2d2651b
tomasVergara14 <tomasvergara272@gmail.com>
2021-07-25 18:03:01 -0300

    data

--- src/data/Coffe.js
+++ src/data/Coffe.js
@@ -10,8 +10,8 @@ const Coffe = [
         "gr":"250 gr",
         "place":"Colombia",
         "price":200,
-        "origen":"GARZÓN, HUILA, COLOMBIA",
-        "altura":"1800-2000 MSNM",
+        "origin":"GARZÓN, HUILA, COLOMBIA",
+        "height":"1800-2000 MSNM",
         "description":"blend regional compuesto por las variedades Caturra, Colombia, Typica. Proviene del pueblo Garzón, en el Departamento del Huila, Colombia. A 1.800 y 2.000 msnm este café creció en un suelo volcánico y fue lavado y secado al sol. En taza tiene mucho dulzor que recuerda a la miel, cacao, notas florales y herbales.",
         "img": arabica
     },
@@ -21,8 +21,8 @@ const Coffe = [
         "gr":"250 gr",
         "place":"Centro America",
         "price":200,
-        "origen":"GARZÓN, HUILA, COLOMBIA",
-        "altura":"1800-2000 MSNM",
+        "origin":"GARZÓN, HUILA, COLOMBIA",
+        "height":"1800-2000 MSNM",
         "description":"blend regional compuesto por las variedades Caturra, Colombia, Typica. Proviene del pueblo Garzón, en el Departamento del Huila, Colombia. A 1.800 y 2.000 msnm este café creció en un suelo volcánico y fue lavado y secado al sol. En taza tiene mucho dulzor que recuerda a la miel, cacao, notas florales y herbales.",
         "img": arabica
     },
@@ -32,8 +32,8 @@ const Coffe = [
         "gr":"250 gr",
         "place": "Brasil",
         "price":200,
-        "origen":"CERRADO MINEIRO, MINAS GERAIS",
-        "altura":"950-1200 MSNM",
+        "origin":"CERRADO MINEIRO, MINAS GERAIS",
+        "height":"950-1200 MSNM",
         "description":"Proviene del cerrado mineiro , en Minas Gerais, BRASIL. A 950-1200 MSNM este café creció en un suelo volcánico y fue lavado y secado al sol. En taza tiene mucho dulzor que recuerda a la miel, cacao, notas florales y herbales.",
         "img": arabica
     }
