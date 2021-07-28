83d2a2fed23da5072d3b7d69611fb4a5e27ac38d
tomasVergara14 <tomasvergara272@gmail.com>
2021-07-28 00:03:36 -0300

    Eleccion de item :id

--- src/components/Main/ItemList/ItemList.jsx
+++ src/components/Main/ItemList/ItemList.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import  Item  from '../Item/Item'
 import Coffe from '../../../data/Coffe'
 import './ItemList.css'
@@ -9,29 +10,38 @@ const ItemList = () => {
     
     const [item, setItem] = useState([])
     const CoffeData = Coffe
-    const {} = useParams()
+    const {itemId} = useParams()
     
-    function handlerPromise(){
-        setTimeout(()=>{
-            Promise.resolve(CoffeData)
-            .then(response =>setItem(response) )
-        }, 2000)
-    }
     
     useEffect(()=>{
-        handlerPromise()
-    },[])
+        setTimeout(()=>{
+            Promise.resolve(CoffeData)
+            .then(res =>{
+                if(itemId === undefined){
+                    setItem(res)
+                }
+                else{
+                    setItem(res.filter(it=> it.id === itemId))
+                }
+            } ) 
+        }, 2000)
+    
+    },[itemId])
 
 
     return (
         <div className="containerItemBox" >
             <h3>Productos</h3>
             <div className="containerItem">
+               
                 {item.map((link, index)=>{
                     const {id, name, place,price , img, description}= link
                     return(
-                        <Item key={index}
-                        img={img} name={name} price={price} description={description} />
+                        <NavLink className="containerItem" to={`/detail/${id}`}>
+                            <Item key={index}
+                            img={img} name={name} price={price} description={description} />
+                        </NavLink>
+                        
                     )   
                     })}
                 
