ca81272115f7f945c2d298d174aa013cd04ac66b
tomasVergara14 <tomasvergara272@gmail.com>
2021-08-05 18:19:16 -0300

    Add items

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,26 +2,45 @@ import React, {useEffect, useState} from 'react'
 import { useCartContext } from '../../handlers/Context/CartContext'
 import { useQuantityContext } from '../../handlers/Context/QuantityContext'
 import { useProductContext } from '../../handlers/Context/ProductContext'
+import CartProduct from './CartProduct'
 
 const Cart = () => {
 
-    const [cartState, setCartState] = useState("No hay productos en el carrito")
+    const [cartState, setCartState] = useState([])
 
     const {Cart, guardarItem} = useCartContext()
     const QuantityDetailContext = useQuantityContext()
     const ProductDetailContext = useProductContext()
 
     const product = ProductDetailContext.product
-    const quantity = QuantityDetailContext.quantity
-    const cartArray = []
 
     console.log(Cart)
+
+    useEffect(() => {
+        if(Cart.length !==0){
+            setCartState(
+                Cart.map((element, index)=>{
+                    const {id, name, price, img}=element 
+                    return(
+                        
+                        <>
+                        <CartProduct key={element.product[0].id} name={element.product[0].name} price={element.product[0].price} img={element.product[0].img} />
+                        </>
+                    )
+                    
+                })
+            )
+        }
+        console.log(cartState.length)
+    }, [product])
+    
 
 
     //Ver por que no funciona
     return (
         <div>
             <h3>Cart</h3>
+            {cartState.length !==0 ? cartState: <p>No hay productos en el carrito</p>}
         </div>
     )
 }
---
+++ src/components/Cart/CartProduct.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './CartProduct.css'
+
+
+
+const CartProduct = (props) => {
+    
+    const {name, price, img}= props
+    return (
+            <div className="CartDetail">         
+                <img className="CartImg" src={img} alt="" />
+                <div className="CartText">
+                    <p>Nombre: {name} </p>
+                    <p>Precio: {price} </p>
+                </div>
+            
+            </div>
+        
+        
+    )
+}
+
+export default CartProduct
+
+
