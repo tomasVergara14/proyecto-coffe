7e809baf777671185563cbe428b6060278aa2e09
tomasVergara14 <tomasvergara272@gmail.com>
2021-08-13 00:23:13 -0300

    category firestore funcionando

--- src/components/Main/category/Category.jsx
+++ src/components/Main/category/Category.jsx
@@ -35,26 +35,32 @@ const Category = () => {
                 setCategory(response.docs.map(cat=>(
                     {...cat.data(), id:cat.id} 
                 )))
+            }else{
+                const categoryFilter = response.docs.filter(cat => cat.data().categoryId === idCategory )
+                setCategory(categoryFilter[0].data())
             }
-            // setCategory(response.docs.find(cat=>{}))
             }
             )
             console.log(category)
         console.log(idCategory)
-},[])
+},[idCategory])
     
     return (
-        <p></p>
-        // <div className="item">
-        //     {category.map((categories)=>{
-        //         const { categoryId, image, title, id}= categories
-        //         return (
-        //             <div key={id} className="itemBox" > 
-        //                 <NavLink to={`${url}${categories.categoryId}`} className="category" key={id}> <img src={image} alt="Imagen de granos de cafe" className="img" /> <p className="product" >{title} </p>  </NavLink>
-        //             </div>
-        //         )
-        //     } )}
-        // </div>
+        <div className="item">
+            {category.length >1 ?
+            category.map((categories)=>{
+                const { categoryId, image, title, id}= categories
+                return (
+                    <div key={id} className="itemBox" > 
+                        <NavLink to={`${url}${categories.categoryId}`} className="category" key={id}> <img src={image} alt="Imagen de granos de cafe" className="img" /> <p className="product" >{title} </p>  </NavLink>
+                    </div>
+                )
+            } )
+            :   <div key={category.id} className="itemBox" > 
+                    <NavLink to={`${url}${category.categoryId}`} className="category" key={category.id}> <img src={category.image} alt="Imagen de granos de cafe" className="img" /> <p className="product" >{category.title} </p>  </NavLink>
+                </div>
+            }
+        </div>
     )
 }
 
