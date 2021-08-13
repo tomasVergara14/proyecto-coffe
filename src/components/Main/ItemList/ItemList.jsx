98b8a602a3dd80a1e7f923a3173f4d9b3ad52e64
tomasVergara14 <tomasvergara272@gmail.com>
2021-08-13 12:32:18 -0300

    item segun category

--- src/components/Main/ItemList/ItemList.jsx
+++ src/components/Main/ItemList/ItemList.jsx
@@ -13,25 +13,41 @@ const ItemList = () => {
     const {idCategory}=useParams()
     const {itemId} = useParams()
 
-    const data = DefineCategory(idCategory)
+    const products = DefineCategory(idCategory)
+    // const res = products.get()
+    // .then(response=>console.log(response.docs[0].data()))
     
     //Category y lista de productos de ese category
     useEffect(()=>{
-        setLoading(true)
-        setTimeout(()=>{
-            Promise.resolve(data) 
-            .then(res =>{
-                if(itemId === undefined){
-                    setLoading(false)
-                    setItem(DefineCategory(idCategory))
+        // setLoading(true)
+        // setTimeout(()=>{
+        //     Promise.resolve(data) 
+        //     .then(res =>{
+        //         if(itemId === undefined){
+        //             setLoading(false)
+        //             setItem(DefineCategory(idCategory))
 
-                }
-                else{
-                    setLoading(false)
-                    setItem(res.filter(it=> it.id === itemId))        
-                }
-            } ) 
-        }, 5000)
+        //         }
+        //         else{
+        //             setLoading(false)
+        //             setItem(res.filter(it=> it.id === itemId))        
+        //         }
+        //     } ) 
+        // }, 5000)
+        const productsData = products.get()
+        .then(response=>{
+            console.log(response.docs[1].data())
+            console.log(item)
+            if(itemId===undefined){
+                setItem(response.docs.map(itm=>(
+                    {...itm.data(), id:itm.id}
+                )))
+            }else{
+                const productFilter = response.docs.filter(itm => itm.data().id === itemId )
+                setItem(productFilter[0].data())
+            }
+            console.log(item)
+        })
     
     },[itemId])
 
@@ -39,22 +55,22 @@ const ItemList = () => {
     return (
         <div className="containerItemBox" >
             <h3>Productos</h3>
-            {/* <div className="container">
+            <div className="container">
                 {loading && <div className="spinner"></div>}
                 {item.map((link, index)=>{
-                    const {id, name, place,price , img, description}= link
+                    const {id, title, price , image, description}= link
                     return(
                         
                         <NavLink className="containerItem" to={`/item/${id}`}>
                             <Item className="itemProduct" key={index}
-                            img={img} name={name} price={price} description={description} />
+                            img={image} name={title} price={price} description={description} />
                         </NavLink>
                         
                     )   
                     })}
                 
             
-            </div> */}
+            </div>
              
         </div>
     )
--- src/handlers/DefineCategory.js
+++ src/handlers/DefineCategory.js
@@ -4,21 +4,21 @@ import getPressInfo from './getPressInfo'
 import getMokaInfo from './getMokaInfo'
 import getExpressoInfo from './getExpressoInfo'
 
-const DefineCategory = (idProduct)=>{
+const DefineCategory = (categoriesId)=>{
     
     //Fijarse qeu no funciona el hook de useParams
     // const {id} = useParams()
 
-        if(idProduct==="1"){
+        if(categoriesId==="1"){
             return(getCoffeInfo)
         }
-        else if(idProduct==="2"){
+        else if(categoriesId==="2"){
             return(getPressInfo)
         }
-        else if(idProduct==="3"){
+        else if(categoriesId==="3"){
             return(getMokaInfo)
         }
-        else if(idProduct==="4"){
+        else if(categoriesId==="4"){
             return(getExpressoInfo)
         }
         else{
