b8398558c009369553700661762aec3ddfc48f65
tomasVergara14 <tomasvergara272@gmail.com>
2021-08-28 10:34:14 -0300

    email ok

--- src/components/Main/Contact/SendEmail/SendEmailForm.jsx
+++ src/components/Main/Contact/SendEmail/SendEmailForm.jsx
@@ -6,24 +6,28 @@ const SendEmailForm = () => {
     function sendEmail(e) {
         e.preventDefault();
     
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+        emailjs.sendForm('gmailTemplate_react', 'template_g8aqs6d', e.target, "user_2ekQ7iS2iExODDxugREGx")
           .then((result) => {
               console.log(result.text);
           }, (error) => {
               console.log(error.text);
           });
+          e.target.reset()
       }
     
       return (
         <form className="contact-form" onSubmit={sendEmail}>
-          <input type="hidden" name="contact_number" />
-          <label>Name</label>
-          <input type="text" name="user_name" />
-          <label>Email</label>
-          <input type="email" name="user_email" />
-          <label>Message</label>
-          <textarea name="message" />
-          <input type="submit" value="Send" />
+          
+          <label className="ContactLabel" >Nombre</label>
+          <input className="ContactInput" type="text" name="user_name" />
+          <label className="ContactLabel" >Email</label>
+          <input className="ContactInput" type="email" name="user_email" />
+          <label className="ContactLabel" >Numero de contacto</label>
+          <input className="ContactInput" type="text" name="contact_number" />
+          <label className="ContactLabel" >Message</label>
+          <br />
+          <textarea className="ContactTextArea" name="message" rows="5"  />
+          <input className="ContactSendButton" type="submit" value="Send" />
         </form>
       );
 }
